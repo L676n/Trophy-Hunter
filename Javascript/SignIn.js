e365fa80f3515cbe22459a50f2e625f976572f18
L676n <123578060+L676n@users.noreply.github.com>
2024-09-23 17:02:55 +0300

    Add files via upload

--- Javascript/SignIn.js
+++ Javascript/SignIn.js
@@ -1,10 +1,4 @@
- // Import the functions you need from the SDKs you need
 
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
-
- // Your web app's Firebase configuration
- //add it from firebase
 
   // Initialize Firebase
  const app = initializeApp(firebaseConfig);
